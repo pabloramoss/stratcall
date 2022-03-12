8c3d59295677410b85e59a3d0b2c38fe1511b6ab
Pablo Ramos <pablo-362@hotmail.com>
2022-03-12 09:17:28 -0300

    fixed donation images

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -9,8 +9,8 @@ const ContactMe = ()=> {
   return (
     <Flex alignItems="center" direction="column">
       <Heading py="30px" color="white" width={{base: "250px", sm:"400px"}} as="h4" size="md" >Si tienes alguna micro que consideres interesante puedes enviarme el link por MD.</Heading>
-      <Link href="https://twitter.com/messages/compose?recipient_id=1466505334898376708&text=Too%20rukoo" target="_blank">
-      <Button _hover={{bg:"#63B3ED"}} bg="#0C7ABF" color="white"><Icon pe="5px" as={FaTwitter} w={6} h={6} color="white" />Contáctame</Button>
+      <Link _hover={{textDecoration: "none"}} href="https://twitter.com/messages/compose?recipient_id=1466505334898376708&text=Too%20rukoo" target="_blank">
+        <Button _hover={{bg:"#63B3ED"}} bg="#0C7ABF" color="white"><Icon pe="5px" as={FaTwitter} w={6} h={6} color="white" />Contáctame</Button>
       </Link>
     </Flex>
   );
